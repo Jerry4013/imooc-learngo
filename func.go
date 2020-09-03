b638275dd4d179533c3034de108a15441cca7c4e
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-03 17:21:46 -0400

    2-6 函数

---
+++ func.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
+
+func eval(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		q, _ := div(a, b)
+		return q, nil
+	default:
+		return 0, fmt.Errorf("unsupported operation: %s", op)
+	}
+}
+
+// 13 / 3 = 4 ... 1
+func div(a, b int) (q, r int) {
+	return a / b, a % b
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args (%d, %d)\n", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func sum(numbers ...int) int {
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
+}
+
+func main() {
+	if result, err := eval(3, 4, "x"); err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println(result)
+	}
+
+	q, r := div(13, 3)
+	fmt.Println(q, r)
+
+	fmt.Println(apply(
+		func(a int, b int) int {
+			return int(math.Pow(float64(a), float64(b)))
+		}, 3, 4))
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
+}
