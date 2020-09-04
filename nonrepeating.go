447f0996fbbda8059125d7d85fadf333578061e7
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-04 12:40:27 -0400

    3-6字符和字符串处理

--- nonrepeating.go
+++ nonrepeating.go
@@ -5,7 +5,7 @@ import "fmt"
 func myLengthOfNonRepeatingSubStr(s string) int {
 	dict := make(map[rune]int)
 	start, max := 0, 0
-	for i, v := range s {
+	for i, v := range []rune(s) {
 		if dict[v] > start {
 			start = dict[v]
 		}
@@ -18,10 +18,10 @@ func myLengthOfNonRepeatingSubStr(s string) int {
 }
 
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccurred := make(map[byte]int)
+	lastOccurred := make(map[rune]int)
 	start := 0
 	maxLength := 0
-	for i, ch := range []byte(s) {
+	for i, ch := range []rune(s) {
 		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
 			start = lastI + 1
 		}
@@ -42,4 +42,5 @@ func main() {
 	fmt.Println(myLengthOfNonRepeatingSubStr("abcdef"))
 	fmt.Println(myLengthOfNonRepeatingSubStr("这里是慕课网"))
 	fmt.Println(myLengthOfNonRepeatingSubStr("一二三二一"))
+	fmt.Println(myLengthOfNonRepeatingSubStr("黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"))
 }
