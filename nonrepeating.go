2577e37946540cbbe9a11cf4e72227539e3e2115
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-04 11:56:31 -0400

    3-5 Map例题

---
+++ nonrepeating.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func myLengthOfNonRepeatingSubStr(s string) int {
+	dict := make(map[rune]int)
+	start, max := 0, 0
+	for i, v := range s {
+		if dict[v] > start {
+			start = dict[v]
+		}
+		if i-start+1 > max {
+			max = i - start + 1
+		}
+		dict[v] = i + 1
+	}
+	return max
+}
+
+func lengthOfNonRepeatingSubStr(s string) int {
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+	for i, ch := range []byte(s) {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+	return maxLength
+}
+
+func main() {
+	fmt.Println(myLengthOfNonRepeatingSubStr("abcabcbb"))
+	fmt.Println(myLengthOfNonRepeatingSubStr("bbbbb"))
+	fmt.Println(myLengthOfNonRepeatingSubStr("pwwkew"))
+	fmt.Println(myLengthOfNonRepeatingSubStr(""))
+	fmt.Println(myLengthOfNonRepeatingSubStr("b"))
+	fmt.Println(myLengthOfNonRepeatingSubStr("abcdef"))
+	fmt.Println(myLengthOfNonRepeatingSubStr("这里是慕课网"))
+	fmt.Println(myLengthOfNonRepeatingSubStr("一二三二一"))
+}
