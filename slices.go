118ce84c61304c479f565f394860c96a1301ed52
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-04 09:54:32 -0400

    3-3切片的操作

--- slices.go
+++ slices.go
@@ -35,8 +35,14 @@ func main() {
 	arr[0], arr[2] = 0, 2
 	fmt.Println("arr = ", arr)
 	s1 = arr[2:6]
-	s2 = s1[3:5]
+	s2 = s1[3:5] // [s1[3], s1[4]]
 	fmt.Printf("s1 =%v, len(s1) = %d, cap(s1) = %d\n", s1, len(s1), cap(s1))
 	fmt.Printf("s2 =%v, len(s2) = %d, cap(s2) = %d\n", s2, len(s2), cap(s2))
-	fmt.Println(s1[3:6])
+
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+	fmt.Println("s3, s4, s5 =", s3, s4, s5)
+	// s4 and s5 no longer view arr.
+	fmt.Println("arr =", arr)
 }
