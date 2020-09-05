dff14b22d057a9d60e324b52eb27368152c080fb
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-05 17:57:05 -0400

    4-3扩展已有类型

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -1,6 +1,24 @@
 package main
 
-import "learngo/tree"
+import (
+	"fmt"
+	"learngo/tree"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
 
 func main() {
 	var root tree.Node
@@ -13,4 +31,8 @@ func main() {
 	root.Right.Left.SetValue(4)
 
 	root.Traverse()
+	fmt.Println()
+	myRoot := myTreeNode{&root}
+	myRoot.postOrder()
+	fmt.Println()
 }
