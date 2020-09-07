45ebe814c3067ec565b9fa27cd9b55fb2429d93b
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-06 20:01:21 -0400

    6-4接口的值类型

--- retriever/main.go
+++ retriever/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"learngo/retriever/mock"
 	"learngo/retriever/real"
+	"time"
 )
 
 type Retriever interface {
@@ -16,7 +17,32 @@ func download(r Retriever) string {
 
 func main() {
 	var r Retriever
+
 	r = mock.Retriever{Contents: "this is a fake imooc.com"}
-	r = real.Retriever{}
-	fmt.Println(download(r))
+	inspect(r)
+
+	r = &real.Retriever{
+		UserAgent: "Mozilla/5.0",
+		TimeOut:   time.Minute,
+	}
+	inspect(r)
+
+	// Type assertion
+	if mockRetriever, ok := r.(mock.Retriever); ok {
+		fmt.Println(mockRetriever.Contents)
+	} else {
+		fmt.Println("not a mock retriever")
+	}
+
+	// fmt.Println(download(r))
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	switch v := r.(type) {
+	case mock.Retriever:
+		fmt.Println("Contents:", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent:", v.UserAgent)
+	}
 }
