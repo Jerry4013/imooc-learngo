c4849d389138fcc33a127e6cdad7272eae788ffc
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-10 20:41:52 -0400

    9-3使用pprof进行性能测试

--- container/nonrepeating/nonrepeating.go
+++ container/nonrepeating/nonrepeating.go
@@ -17,12 +17,21 @@ import "fmt"
 //	return max
 //}
 
+var lastOccurred = make([]int, 0xffff) // 65535
+
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccurred := make(map[rune]int)
+
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
+	// lastOccurred[0x65] = 1
+	// lastOccurred[0x8BFE] = 6
+
 	start := 0
 	maxLength := 0
+
 	for i, ch := range []rune(s) {
-		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
 			start = lastI + 1
 		}
 		if i-start+1 > maxLength {
