99a22ca2ebc12ae64c724b17bc16e23168319ea6
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-11 11:31:17 -0400

    9-6生成文档和示例代码

--- queue/queue.go
+++ queue/queue.go
@@ -1,17 +1,22 @@
 package queue
 
+// An FIFO queue
 type Queue []interface{}
 
+// Pushes the element into the queue.
+// 		e.g. q.Push(123)
 func (q *Queue) Push(v interface{}) {
 	*q = append(*q, v.(int))
 }
 
+// Pops element from head
 func (q *Queue) Pop() interface{} {
 	head := (*q)[0]
 	*q = (*q)[1:]
 	return head.(int)
 }
 
+// Returns if the queue is empty or not.
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
