1e36679d3b6ea3eef5663df06e6f20d47e097854
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-14 12:28:18 -0400

    11-2使用Channel等待任务结束

--- channel/done/done.go
+++ channel/done/done.go
@@ -2,50 +2,49 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func doWork(id int, c chan int, done chan bool) {
-	for n := range c {
+func doWork(id int, w worker) {
+	for n := range w.in {
 		fmt.Printf("Worker %d received %c\n", id, n)
-		go func() {
-			done <- true
-		}()
+		w.done()
 	}
 }
 
 type worker struct {
 	in   chan int
-	done chan bool
+	done func()
 }
 
-func createWorker(id int) worker {
+func createWorker(id int, wg *sync.WaitGroup) worker {
 	w := worker{
-		in:   make(chan int),
-		done: make(chan bool),
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
 	}
-	go doWork(id, w.in, w.done)
+	go doWork(id, w)
 	return w
 }
 
 func chanDemo() {
+	var wg sync.WaitGroup
+
 	var workers [10]worker
 	for i := 0; i < 10; i++ {
-		workers[i] = createWorker(i)
+		workers[i] = createWorker(i, &wg)
 	}
 
+	wg.Add(20)
 	for i, worker := range workers {
 		worker.in <- 'a' + i
 	}
-
 	for i, worker := range workers {
 		worker.in <- 'A' + i
 	}
 
-	// wait for all of them
-	for _, worker := range workers {
-		<-worker.done
-		<-worker.done
-	}
+	wg.Wait()
 }
 
 func main() {
