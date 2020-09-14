3b775ee0c116bee65d828d3a7ecfd4b0580daec1
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-14 15:03:58 -0400

    11-4 hasValue

--- channel/select/select.go
+++ channel/select/select.go
@@ -19,14 +19,37 @@ func generator() chan int {
 	return out
 }
 
+func worker(id int, c chan int) {
+	for n := range c {
+		fmt.Printf("Worker %d received %d\n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
 func main() {
 	var c1, c2 = generator(), generator()
+	var worker = createWorker(0)
+
+	n := 0
+	hasValue := false
 	for {
+		var activeWorker chan<- int
+		if hasValue {
+			activeWorker = worker
+		}
+
 		select {
-		case n := <-c1:
-			fmt.Println("Received from c1:", n)
-		case n := <-c2:
-			fmt.Println("Received from c2:", n)
+		case n = <-c1:
+			hasValue = true
+		case n = <-c2:
+			hasValue = true
+		case activeWorker <- n:
+			hasValue = false
 		}
 	}
 }
