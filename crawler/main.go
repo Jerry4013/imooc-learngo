a0a749afeec841133821eea430e131bf843062f3
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-18 16:51:51 -0400

    15-3 city and URL

--- crawler/main.go
+++ crawler/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 
 	"golang.org/x/text/transform"
 
@@ -32,7 +33,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", all)
+	printCityList(all)
 }
 
 func determineEncoding(r io.Reader) encoding.Encoding {
@@ -42,4 +43,13 @@ func determineEncoding(r io.Reader) encoding.Encoding {
 	}
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
+}
+
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[a-z0-9]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+	}
+	fmt.Printf("Matches found: %d\n", len(matches))
 }
