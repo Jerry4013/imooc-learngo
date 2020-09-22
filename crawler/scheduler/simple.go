3e569cbada6e569e2fceec0c979e9ff267dc589e
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-22 17:38:46 -0400

    16-5 refactor

--- crawler/scheduler/simple.go
+++ crawler/scheduler/simple.go
@@ -8,8 +8,15 @@ type SimpleScheduler struct {
 	workerChan chan engine.Request
 }
 
-func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
-	s.workerChan = c
+func (s *SimpleScheduler) WorkerChan() chan engine.Request {
+	return s.workerChan
+}
+
+func (s *SimpleScheduler) WorkerReady(requests chan engine.Request) {
+}
+
+func (s *SimpleScheduler) Run() {
+	s.workerChan = make(chan engine.Request)
 }
 
 func (s *SimpleScheduler) Submit(request engine.Request) {
