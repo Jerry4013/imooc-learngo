902f8181f80af5a779b3caed4093e07bb1921c39
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-09-21 22:50:14 -0400

    16-3 concurrent scheduler

--- crawler/main.go
+++ crawler/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"learngo/crawler/engine"
+	"learngo/crawler/scheduler"
 	"learngo/crawler/zhenai/parser"
 )
 
 func main() {
-	engine.Run(engine.Request{
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 100,
+	}
+
+	e.Run(engine.Request{
 		Url:        "http://localhost:8080/mock/www.zhenai.com/zhenghun",
 		ParserFunc: parser.ParseCityList,
 	})
---
+++ crawler/scheduler/simple.go
@@ -0,0 +1,18 @@
+package scheduler
+
+import (
+	"learngo/crawler/engine"
+)
+
+type SimpleScheduler struct {
+	workerChan chan engine.Request
+}
+
+func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
+	s.workerChan = c
+}
+
+func (s *SimpleScheduler) Submit(request engine.Request) {
+	// send request down to worker chan
+	go func() { s.workerChan <- request }()
+}
